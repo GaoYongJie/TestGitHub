3986cf40d98d8875c7be512b9327d21ecb613380
高永杰 <1034015878@qq.com>
2018-01-02 15:22:01 +0800

    第一次提交

---
+++ TestGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGitHub
+//
+//  Created by 高永杰 on 2018/1/2.
+//  Copyright © 2018年 GYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGitHub
+//
+//  Created by 高永杰 on 2018/1/2.
+//  Copyright © 2018年 GYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
